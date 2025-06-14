1d4b18ca250477c370394574589712c2e92e7af0
Harshil Goti <hgoti@plusonex.com>
2025-06-14 14:24:03 +0530

    small changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { store } from "./store/store.ts";
 import { Provider } from "react-redux";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <App />
     </Provider>
   </StrictMode>
 );
